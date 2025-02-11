98940a04e2529bbadd588e21561aedefbd264b90
Julien <larrieu.julien8@gmail.com>
2025-02-11 13:47:59 +0000

    margin and purchase_cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,20 @@
+WITH sq AS
+--Query 1
+(
+ SELECT
+    a.date_date as date_date,
+    a.orders_id as orders_id,
+    a.revenue,
+    b.purchase_price*a.quantity as purchase_cost
+FROM {{ ref('stg_raw__sales') }} as a
+INNER JOIN {{ ref('stg_raw__product') }} as b
+ ON a.products_id = b.products_id
+ 
+)
+--Query 2
+SELECT
+    date_date,
+    orders_id,
+    ROUND(revenue-purchase_cost,2) as margin,
+    ROUND(purchase_cost,2) as purchase_cost,
+ FROM sq
