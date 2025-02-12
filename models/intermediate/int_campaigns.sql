1555ea4154402678497ff8e75d0316945e742d45
JULIEN LARRIEU <larrieu.julien8@gmail.com>
2025-02-12 15:07:19 +0000

    yaml

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -11,3 +11,12 @@
   UNION ALL
  SELECT *
  FROM {{ref('stg_raw__facebook')}}
+
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adwords'),
+               ref('stg_raw__bing'),
+               ref('stg_raw__criteo'),
+               ref('stg_raw__facebook'),              
+    ],
+
+) }}
