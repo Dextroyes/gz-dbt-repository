8c1a6ab6eabbd698b74d8a770e3cbc97ff07c5fe
JULIEN LARRIEU <larrieu.julien8@gmail.com>
2025-02-12 10:25:48 +0000

    solution cleared

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,10 +1,17 @@
-SELECT 
-    a.orders_id,
-    a.date_date,
-    ROUND(a.margin + b.shipping_fee - b.logcost - b.ship_cost,2) as operational_margin,
-    a.quantity,
-    a.revenue
-FROM {{ ref('int_sales_margin') }} as a
-INNER JOIN {{ ref('stg_raw__ship') }} as b
- ON a.orders_id = b.orders_id
-ORDER BY orders_id DESC
+ -- int_orders_operational.sql
+
+ SELECT
+     o.orders_id
+     ,o.date_date
+     ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,23 +1,14 @@
-WITH sq AS
---Query 1
-(
- SELECT
-    a.date_date as date_date,
-    a.orders_id as orders_id,
-    a.revenue as revenue,
-    a.quantity as quantity,
-    b.purchase_price*a.quantity as purchase_cost
-FROM {{ ref('stg_raw__sales') }} as a
-INNER JOIN {{ ref('stg_raw__product') }} as b
- ON a.products_id = b.products_id
- 
-)
---Query 2
-SELECT
-    orders_id,
-    date_date,
-    revenue,
-    quantity,
-    ROUND(purchase_cost,2) as purchase_cost,
-    ROUND(revenue-purchase_cost,2) as margin,
- FROM sq
+  -- int_sales_margin.sql
+
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
